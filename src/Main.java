36463c3e48b391b15e377146285547701471d412
IT <miami2016@bk.ru>
2022-02-02 21:13:00 +0300

    Half Life 3

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Player> players = new ArrayList<>();
+
+        Player gordon = new Player("Gordon Freeman");
+        players.add(gordon);
+
+        Main.load("Gordon Freeman");
+
+        System.out.println(gordon);
+
+        gordon.setArmor(100);
+        Main.save(gordon);
+        Main.load("Gordon Freeman");
+
+        System.out.println(gordon);
+
+    }
+
+    private static void save(Player player) {
+        File file = new File(player.getName() + ".save");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            if (!file.exists()) file.createNewFile();
+            oos.writeObject(player);
+        } catch (IOException e) {
+            System.out.println("Не удается сохраненить игру!");
+        }
+    }
+
+    private static Player load(String playerName) {
+        File file = new File(playerName + ".save");
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            Player player = (Player) ois.readObject();
+            return player;
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Не удалось загрузить игру!");
+            return null;
+        }
+    }
+}
